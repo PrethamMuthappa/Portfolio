c448a410819e1388421ba141c5b792e3e0f44a6f
pretham <prethambachira1819@gmail.com>
2026-09-17 19:15:47 +0530

    version update

--- astro.config.js
+++ astro.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
-
+import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
-	integrations: [tailwind()],
-	output: 'server',
+	vite: {
+		plugins: [tailwindcss()]
+	},
+	output: "server",
 	adapter: vercel({
  		webAnalytics: { enabled: true }
 	}),
 	site: "https://prethambachira.vercel.app"
 });
-
